aef7ac4e55b0dbf61a341f76b6dd1aec5a5eb2ad
Hayato Ito <hayatoito@gmail.com>
2023-08-24 10:55:22 +0900

    Add expreg from elpa

---
+++ elpa/expreg-1.0.0/expreg-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from expreg.el  -*- no-byte-compile: t -*-
+(define-package "expreg" "1.0.0" "Simple expand region" '((emacs "29.1")) :commit "a5ceac5c0c646cf1a2ea674465be3726e272e675" :authors '(("Yuan Fu" . "casouri@gmail.com")) :maintainer '("Yuan Fu" . "casouri@gmail.com") :keywords '("text" "editing") :url "https://github.com/casouri/expreg")
